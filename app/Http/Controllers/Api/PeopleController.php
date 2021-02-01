870ad159bafd7e1bbcfac81a36ba312989df0330
Vitor Carvalho <lightningspirit@gmail.com>
2021-02-01 21:39:57 +0000

    Aula 2021-02-01

--- app/Http/Controllers/Api/PeopleController.php
+++ app/Http/Controllers/Api/PeopleController.php
@@ -13,13 +13,34 @@ class PeopleController extends Controller
     $per_page = (int) $request->query('per_page', 5);
     $page = (int) $request->query('page', 1);
 
-    $result = Person::all();
+    $query = Person::query();
+
+    $total = $query->count();
+    $skip = ($page * $per_page) - $per_page;
+
+    $result = $query->skip($skip)->take($per_page)->get();
+
+    // $total: 11
+    // $per_page: 5
+    // $page: 3
+    // $skip = 10
+    // $skip + $per_page = 15 = $page * $per_page - $per_page + $per_page
+
+    $links = [
+      'self' => "/api/v1/people?page=$page",
+    ];
+
+    if ($page > 1) {
+      $links['previous'] = "/api/v1/people?page=".($page-1);
+    }
+
+    if ($page * $per_page < $total) {
+      $links['next'] = "/api/v1/people?page=".($page+1);
+    }
 
     return response()
       ->json([
-        '_links' => [
-          'self' => '/api/v1/people'
-        ],
+        '_links' => $links,
         '_embedded' => [
           'people' => $result
         ]
